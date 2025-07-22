f7763a835d1beb62ccf0ecb85b42363673097772
sdrummond15 <silvio@sdrummond.com.br>
2025-07-22 14:54:45 -0300

    banners internos

--- administrator/logs/error.php
+++ administrator/logs/error.php
@@ -5,3 +5,5 @@
 
 #Fields: datetime	priority clientip	category	message
 2025-07-03T20:48:37+00:00	INFO ::1	joomlafailure	Não é permitido senha vazia.
+2025-07-21T17:28:49+00:00	INFO ::1	joomlafailure	Não é permitido senha vazia.
+2025-07-21T19:25:51+00:00	INFO ::1	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
